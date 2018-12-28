45d69529802a2b48e81f6190974e231f8341628e
Salvador Aveldano <salvador.aveldano@gmail.com>
2018-12-28 01:52:39 -0600

    Change in the use of limits for the plot

--- Exploration.R
+++ Exploration.R
@@ -13,11 +13,15 @@ trips <- oct18 %>%
 
 
 plota <- trips %>%
-  mutate(f = STATION_A-STATION_B) %>% 
+  mutate(f = STATION_A-STATION_B,
+         STATION_A  = STATION_A %>% 
+           as.factor(),
+         STATION_B  = STATION_B %>% 
+           as.factor()) %>% 
   filter(
     f != 0,
-    n > 88,
-    WDAY %in% 1:5
+    n > 1,
+    WDAY %in% 1:7
     ) %>% 
   mutate_at(vars(contains('STATION')),funs(str_squish(.))) %>% 
   ggplot(
@@ -28,6 +32,9 @@ plota <- trips %>%
     )
   ) + 
   geom_tile() +
+  scale_x_discrete(limits = seq(1,190,1)) +
+  scale_y_discrete(limits = seq(1,190,1)) +
+  scale_fill_viridis_c() +
   # facet_wrap(vars(WDAY)) +
   theme(
     axis.text.x = element_text(angle = 0)
@@ -35,5 +42,6 @@ plota <- trips %>%
   coord_flip()
 
 plota
+
 
 plotly::ggplotly(plota)
